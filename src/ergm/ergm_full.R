7903594fb0b65f2afe74e5972d23e1c894742475
tomdewildt <32359630+tomdewildt@users.noreply.github.com>
2021-11-21 20:44:40 +0100

    Add alt ergm script (country)

--- src/ergm/ergm_full.R
+++ src/ergm/ergm_full.R
@@ -1,4 +1,4 @@
-# ERGM
+# ERGM (Full)
 
 ## Set seed
 random_seed <- 42
@@ -65,19 +65,33 @@ network_industry <- intergraph::asNetwork(network_industry)
 ### Degree
 summary(network_climate ~ degree(0:250))
 
+### Triangles
+summary(network_climate ~ triangles)
+
+### KStar
+summary(network_climate ~ kstar(0:250))
+
 ## Check industry terms
 
 ### Degree
 summary(network_industry ~ degree(0:250))
+
+### Triangles
+summary(network_industry ~ triangles)
+
+### KStar
+summary(network_industry ~ kstar(0:250))
 
 ## Create climate ERGM
 
 ### Edges (5sec - CONVERGED)
 m0_climate <- ergm::ergm(network_climate ~ edges)
 
+sink("./models/m0_climate.txt")
 summary(m0_climate)
-
 (m0_climate_fit <- ergm::gof(m0_climate))
+sink()
+
 plot(m0_climate_fit)
 
 saveRDS(m0_climate, file = "./models/m0_climate.rds")
@@ -92,38 +106,70 @@ m1_climate <- ergm::ergm(network_climate ~ edges + isolates,
                                                       parallel = 4),
                          verbose = TRUE)
 
+sink("./models/m1_climate.txt")
 summary(m1_climate)
-
 (m1_climate_fit <- ergm::gof(m1_climate))
+sink()
+
 plot(m1_climate_fit)
 
+sink("./models/m1_climate.txt", append = TRUE)
 ergm::mcmc.diagnostics(m1_climate)
+sink()
 
 saveRDS(m1_climate, file = "./models/m1_climate.rds")
 
 ### Edges + Isolates + GWDegree (5min - CONVERGED)
-m2_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE),
-                         check.degeneracy = TRUE,
-                         control = ergm::control.ergm(MCMC.burnin = 5000,
-                                                      MCMC.samplesize = 10000,
-                                                      seed = random_seed,
-                                                      MCMLE.maxit = 60,
-                                                      parallel = 4),
-                         verbose = TRUE)
+m2_1_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE),
+                           check.degeneracy = TRUE,
+                           control = ergm::control.ergm(MCMC.burnin = 5000,
+                                                        MCMC.samplesize = 10000,
+                                                        seed = random_seed,
+                                                        MCMLE.maxit = 60,
+                                                        parallel = 4),
+                           verbose = TRUE)
 
-summary(m2_climate)
+sink("./models/m2_1_climate.txt")
+summary(m2_1_climate)
+(m2_1_climate_fit <- ergm::gof(m2_1_climate))
+sink()
 
-(m2_climate_fit <- ergm::gof(m2_climate))
-plot(m2_climate_fit)
+plot(m2_1_climate_fit)
 
-ergm::mcmc.diagnostics(m2_climate)
+sink("./models/m2_1_climate.txt", append = TRUE)
+ergm::mcmc.diagnostics(m2_1_climate)
+sink()
 
-saveRDS(m2_climate, file = "./models/m2_climate.rds")
+saveRDS(m2_1_climate, file = "./models/m2_1_climate.rds")
 
-### Edges + Isolates + GWDegree + nodematch("group_abbv") (??? - ???)
+### Edges + Isolates + AltKStar (5min - CONVERGED)
+m2_2_climate <- ergm::ergm(network_climate ~ edges + isolates + altkstar(0.5, fixed = TRUE),
+                           check.degeneracy = TRUE,
+                           control = ergm::control.ergm(MCMC.burnin = 5000,
+                                                        MCMC.samplesize = 10000,
+                                                        seed = random_seed,
+                                                        MCMLE.maxit = 60,
+                                                        parallel = 4),
+                           verbose = TRUE)
+
+sink("./models/m2_2_climate.txt")
+summary(m2_2_climate)
+(m2_2_climate_fit <- ergm::gof(m2_2_climate))
+sink()
+
+plot(m2_2_climate_fit)
+
+sink("./models/m2_2_climate.txt", append = TRUE)
+ergm::mcmc.diagnostics(m2_2_climate)
+sink()
+
+saveRDS(m2_2_climate, file = "./models/m2_2_climate.rds")
+
+### Edges + Isolates + GWDegree + nodematch("group_abbv") (20:00 - ???)
 m3_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE) +
-                         nodematch("group_abbv", diff = TRUE),
+                           nodematch("group_abbv"),
                          check.degeneracy = TRUE,
+                         # control = ergm::control.ergm(parallel = 4),
                          control = ergm::control.ergm(MCMC.burnin = 5000,
                                                       MCMC.samplesize = 10000,
                                                       seed = random_seed,
@@ -131,18 +177,22 @@ m3_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0
                                                       parallel = 4),
                          verbose = TRUE)
 
+sink("./models/m3_climate.txt")
 summary(m3_climate)
+(m3_climate_fit <- ergm::gof(m3_climate))
+sink()
 
-(m3_industry_fit <- ergm::gof(m3_climate))
 plot(m3_climate_fit)
 
+sink("./models/m3_climate.txt", append = TRUE)
 ergm::mcmc.diagnostics(m3_climate)
+sink()
 
 saveRDS(m3_climate, file = "./models/m3_climate.rds")
 
 ### Edges + Isolates + GWDegree + nodematch("country") (??? - ???)
 m4_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE) +
-                         nodematch("country", diff = FALSE),
+                           nodematch("country"),
                          check.degeneracy = TRUE,
                          control = ergm::control.ergm(MCMC.burnin = 5000,
                                                       MCMC.samplesize = 10000,
@@ -151,18 +201,22 @@ m4_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0
                                                       parallel = 4),
                          verbose = TRUE)
 
+sink("./models/m4_climate.txt")
 summary(m4_climate)
+(m4_climate_fit <- ergm::gof(m4_climate))
+sink()
 
-(m4_industry_fit <- ergm::gof(m4_climate))
-plot(m4_climate_fit)
+plot(m3_climate_fit)
 
+sink("./models/m4_climate.txt", append = TRUE)
 ergm::mcmc.diagnostics(m4_climate)
+sink()
 
 saveRDS(m4_climate, file = "./models/m4_climate.rds")
 
 ### Edges + Isolates + GWDegree + nodematch("group_abbv") + nodematch("country") (??? - ???)
 m5_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE) +
-                         nodematch("group_abbv", diff = TRUE) + nodematch("country", diff = FALSE),
+                           nodematch("group_abbv", diff = TRUE) + nodematch("country", diff = FALSE),
                          check.degeneracy = TRUE,
                          control = ergm::control.ergm(MCMC.burnin = 5000,
                                                       MCMC.samplesize = 10000,
@@ -171,12 +225,16 @@ m5_climate <- ergm::ergm(network_climate ~ edges + isolates + gwdegree(decay = 0
                                                       parallel = 4),
                          verbose = TRUE)
 
+sink("./models/m5_climate.txt")
 summary(m5_climate)
-
 (m5_climate_fit <- ergm::gof(m5_climate))
+sink()
+
 plot(m5_climate_fit)
 
+sink("./models/m5_climate.txt", append = TRUE)
 ergm::mcmc.diagnostics(m5_climate)
+sink()
 
 saveRDS(m5_climate, file = "./models/m5_climate.rds")
 
@@ -185,9 +243,11 @@ saveRDS(m5_climate, file = "./models/m5_climate.rds")
 ### Edges (5sec - CONVERGED)
 m0_industry <- ergm::ergm(network_industry ~ edges)
 
+sink("./models/m0_industry.txt")
 summary(m0_industry)
-
 (m0_industry_fit <- ergm::gof(m0_industry))
+sink()
+
 plot(m0_industry_fit)
 
 saveRDS(m0_industry, file = "./models/m0_industry.rds")
@@ -202,37 +262,68 @@ m1_industry <- ergm::ergm(network_industry ~ edges + isolates,
                                                        parallel = 4),
                           verbose = TRUE)
 
+sink("./models/m1_industry.txt")
 summary(m1_industry)
-
 (m1_industry_fit <- ergm::gof(m1_industry))
+sink()
+
 plot(m1_industry_fit)
 
+sink("./models/m1_industry.txt", append = TRUE)
 ergm::mcmc.diagnostics(m1_industry)
+sink()
 
 saveRDS(m1_industry, file = "./models/m1_industry.rds")
 
 ### Edges + Isolates + GWDegree (16hr 15min - NO PROGRESS)
-m2_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE),
-                          check.degeneracy = TRUE,
-                          control = ergm::control.ergm(MCMC.burnin = 5000,
-                                                       MCMC.samplesize = 10000,
-                                                       seed = random_seed,
-                                                       MCMLE.maxit = 60,
-                                                       parallel = 4),
-                          verbose = TRUE)
+m2_1_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE),
+                            check.degeneracy = TRUE,
+                            control = ergm::control.ergm(MCMC.burnin = 5000,
+                                                         MCMC.samplesize = 10000,
+                                                         seed = random_seed,
+                                                         MCMLE.maxit = 60,
+                                                         parallel = 4),
+                            verbose = TRUE)
 
-summary(m2_industry)
+sink("./models/m2_1_industry.txt")
+summary(m2_1_industry)
+(m2_1_industry_fit <- ergm::gof(m2_1_industry))
+sink()
 
-(m2_industry_fit <- ergm::gof(m2_industry))
-plot(m2_industry_fit)
+plot(m2_1_industry_fit)
 
-ergm::mcmc.diagnostics(m2_industry)
+sink("./models/m2_1_industry.txt", append = TRUE)
+ergm::mcmc.diagnostics(m2_1_industry)
+sink()
 
-saveRDS(m2_industry, file = "./models/m2_industry.rds")
+saveRDS(m2_1_industry, file = "./models/m2_1_industry.rds")
+
+### Edges + Isolates + AltKStar (??? - ???)
+m2_2_industry <- ergm::ergm(network_industry ~ edges + isolates + altkstar(0.5, fixed = TRUE),
+                            check.degeneracy = TRUE,
+                            control = ergm::control.ergm(MCMC.burnin = 5000,
+                                                         MCMC.samplesize = 10000,
+                                                         seed = random_seed,
+                                                         MCMLE.maxit = 60,
+                                                         parallel = 4),
+                            verbose = TRUE)
+
+sink("./models/m2_2_industry.txt")
+summary(m2_2_industry)
+(m2_2_industry_fit <- ergm::gof(m2_2_industry))
+sink()
+
+plot(m2_2_industry_fit)
+
+sink("./models/m2_2_industry.txt", append = TRUE)
+ergm::mcmc.diagnostics(m2_2_industry)
+sink()
+
+saveRDS(m2_2_industry, file = "./models/m2_2_industry.rds")
 
 ### Edges + Isolates + GWDegree + nodematch("group_abbv") (??? - ???)
 m3_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE) +
-                          nodematch("group_abbv", diff = TRUE),
+                            nodematch("group_abbv", diff = TRUE),
                           check.degeneracy = TRUE,
                           control = ergm::control.ergm(MCMC.burnin = 5000,
                                                        MCMC.samplesize = 10000,
@@ -241,18 +332,22 @@ m3_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay =
                                                        parallel = 4),
                           verbose = TRUE)
 
+sink("./models/m3_industry.txt")
 summary(m3_industry)
-
 (m3_industry_fit <- ergm::gof(m3_industry))
+sink()
+
 plot(m3_industry_fit)
 
+sink("./models/m3_industry.txt", append = TRUE)
 ergm::mcmc.diagnostics(m3_industry)
+sink()
 
 saveRDS(m3_industry, file = "./models/m3_industry.rds")
 
 ### Edges + Isolates + GWDegree + nodematch("country") (??? - ???)
 m4_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE) +
-                          nodematch("country", diff = FALSE),
+                            nodematch("country", diff = FALSE),
                           check.degeneracy = TRUE,
                           control = ergm::control.ergm(MCMC.burnin = 5000,
                                                        MCMC.samplesize = 10000,
@@ -261,18 +356,22 @@ m4_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay =
                                                        parallel = 4),
                           verbose = TRUE)
 
+sink("./models/m4_industry.txt")
 summary(m4_industry)
-
 (m4_industry_fit <- ergm::gof(m4_industry))
+sink()
+
 plot(m4_industry_fit)
 
+sink("./models/m4_industry.txt", append = TRUE)
 ergm::mcmc.diagnostics(m4_industry)
+sink()
 
 saveRDS(m4_industry, file = "./models/m4_industry.rds")
 
 ### Edges + Isolates + GWDegree + nodematch("group_abbv") + nodematch("country") (??? - ???)
 m5_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay = 0.5, fixed = FALSE) +
-                          nodematch("group_abbv", diff = TRUE) + nodematch("country", diff = FALSE),
+                            nodematch("group_abbv", diff = TRUE) + nodematch("country", diff = FALSE),
                           check.degeneracy = TRUE,
                           control = ergm::control.ergm(MCMC.burnin = 5000,
                                                        MCMC.samplesize = 10000,
@@ -281,11 +380,15 @@ m5_industry <- ergm::ergm(network_industry ~ edges + isolates + gwdegree(decay =
                                                        parallel = 4),
                           verbose = TRUE)
 
+sink("./models/m5_industry.txt")
 summary(m5_industry)
-
 (m5_industry_fit <- ergm::gof(m5_industry))
+sink()
+
 plot(m5_industry_fit)
 
+sink("./models/m5_industry.txt", append = TRUE)
 ergm::mcmc.diagnostics(m5_industry)
+sink()
 
 saveRDS(m5_industry, file = "./models/m5_industry.rds")
